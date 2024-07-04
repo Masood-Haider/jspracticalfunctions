81d00a47bb4c11a122e84a91f006c0d38f54fd41
Masood-Haider <masood.haider.bangash1@gmail.com>
2024-07-04 14:37:07 +0500

    done

---
+++ script.js
@@ -0,0 +1,39 @@
+// let str="  hello world    ";
+// let st=str.trim();
+// console.log(st);
+
+// let s="hello world";
+// let subs=s.substring(5,10);
+// console.log(subs);
+
+// let str="hello world";
+// let c=str.substr(0,5);
+// console.log(c);
+
+// let str="hello-world";
+// let su=str.split("-");
+// console.log(su);
+
+// let ar=["hello","world"];
+// let s=ar.join(" ");
+// console.log(s)
+
+// let str="hello world";
+// let s=str.indexOf("o");
+// console.log(s);
+
+// let str="hello world world";
+// let s=str.lastIndexOf("world");
+// console.log(s);
+
+// let str="world";
+// let s=str.startsWith("wor");
+// console.log(s);
+
+// let str="hello world";
+// let s=str.endsWith("world");
+// console.log(s);
+
+// let str="hello";
+// let s=str.includes("o");
+// console.log(s);
